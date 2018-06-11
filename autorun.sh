5dbb49dfa70170aaad943b47823f19a217e9ed70
mareksamec <samec.marek@gmail.com>
2018-06-11 23:23:54 +0200

    Added keyboard mapping and autorun
    Added keyboard mapping switch via setxkbmap
    Added autorun.sh script to rclua
    Changed client kill to mod + q
    Played a bit with the titelbars

--- autorun.sh
+++ autorun.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
 function run {
-	  if ! pgrep $1 ;
-		    then
-			        $@&
-				  fi
-			  }
+  if ! pgrep $1 ;
+  then
+    $@&
+  fi
+}
+
+run guake -e "tail -f /var/log/syslog"
+run pulseaudio
+run kdeconnect-cli
+run kdeconnect-handler
+
+run gnome-keyring-daemon --daemonize --login
+run gnome-online-accounts
+run megasync
